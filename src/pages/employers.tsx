710c34b624106f2d469b4b450b0e44411aec6f3f
K0Stek122 <kamilianos3@gmail.com>
2026-02-04 15:50:03 +0000

    feat: enhance Employers page with a personal quote, freelance offerings, and detailed service list

--- src/pages/employers.tsx
+++ src/pages/employers.tsx
@@ -34,6 +34,9 @@ const EmployersPage: React.FC<EmployersPageProps> = () => {
                 />
                 <div className="flex flex-col items-center justify-center gap-4 bg-slate-800">
                     <H1 text="Kamil Kostrzewa"></H1>
+                    <p className=" [&:not(:first-child)]: text-white">
+                        "Do More with Less. Be Pragmatic."
+                    </p>
                     <div className="flex flex-row items-center justify-center gap-4 bg-slate-800">
 
                         <Button
@@ -66,11 +69,32 @@ const EmployersPage: React.FC<EmployersPageProps> = () => {
                     </div>
                 </div>
             </div>
-            <Separator className="animate-fadeInUp max-w-3/5" />
-            <div className="animate-fadeInUp flex flex-col items-center w-screen bg-slate-800 max-w-3/5">
+            <Separator className="animate-fadeInUp max-w-2/5" />
+            <div className="animate-fadeInUp flex flex-col items-center w-screen bg-slate-800 max-w-2/5">
+            
                 <H1 text="About Me" />
-                <p className="leading-7 [&:not(:first-child)]:mt-6 text-white">
-                    The Quick Brown Fox Jumps Over The Lazy Dog.
+                <p className="leading-7 [&:not(:first-child)]:mb-6 mt-2 text-white">
+                    Hi! I am an aspiring Software Engineer with a deep-rooted passion for computer science. Since childhood I adored programming and loved creating my own tools. Head over to GitHub to see some of my best projects.
+                </p>
+
+                <H1 text="Freelance & Short-Term Work" />
+                <p className="leading-7 [&:not(:first-child)]:mt-2 text-white">
+                    <b className="underline">Open to freelance.</b> Not Individual but Business-oriented. Below you will find some of the issues I can help fix at your business.
+                </p>
+
+                <ul className="ml-6 list-disc mt-2 text-white">
+                    <li className="mb-2">Data Entry, Analytics & Visualisation.</li>
+                    <li className="mb-2">Application maintenance. Refactoring, Documentation, CI/CD optimisation.</li>
+                    <li className="mb-2">Small-scale Applications (e.g. Internal tools for bookkeeping or data entry)</li>
+                    <li className="mb-2">Small and Medium-Scale Website Development.</li>
+                </ul>
+
+                <p className="leading-7 [&:not(:first-child)]:mb-6 text-white">
+                    <b className="underline">Offering Free Consultations. Charging Per Result.</b> If your issue is not listed here I am open to taking a look at your situation for free to see what we can work out.
+                </p>
+
+                <p className="leading-7 [&:not(:first-child)]:mb-6 text-white">
+                    <b className="underline">Affordable & Reasonable Student Prices.</b>
                 </p>
             </div>
         </div>
