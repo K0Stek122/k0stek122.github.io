b9376b3d31e328625ff357e9e84e1f0abb1e048c
K0Stek122 <kamilianos3@gmail.com>
2026-04-22 18:29:45 +0100

    Added fields for blog search and blog tag search. corrected breadcrumbs for blog pages.
    Signed-off-by: K0Stek122 <kamilianos3@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IndexPage from './pages/index';
 import EmployersPage from './pages/employers';
 import PortfolioPage from './pages/portfolio';
+import BlogPage from './pages/blog';
+import BlogPostPage from './pages/blog-post';
 import './App.css'
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
             <Route path="/" element={<IndexPage />} />
             <Route path="/employers" element={<EmployersPage />} />
             <Route path="/employers/portfolio" element={<PortfolioPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/post" element={<BlogPostPage />} />
         </Routes>
     </BrowserRouter>
   )
